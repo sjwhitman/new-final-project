4f4b7a76428935ce39873ec541ed848309abc29d
Yun Whitman <savannahwhitman1@gmail.com>
2024-01-15 15:37:48 -0800

    Inserted task model to DB

--- db_init.py
+++ db_init.py
@@ -15,31 +15,28 @@ connection = engine.connect()
 
 Base = declarative_base()
 
-class Article(Base):
-    __tablename__ = 'articles'
+class Task(Base):
+    __tablename__ = "tasks"
 
-    id = Column(Integer(), primary_key=True)
-    slug = Column(String(100), nullable=False, unique=True)
-    title = Column(String(100), nullable=False)
-    created_on = Column(DateTime(), default=datetime.now)
-    updated_on = Column(DateTime(), default=datetime.now, onupdate=datetime.now)
-    content = Column(Text)
-    # author_id = Column(Integer(), ForeignKey('authors.id'))
+    task_id = Column(Integer, primary_key=True)
+    name = Column(String(50), nullable=False)
+    start_time = Column(DateTime(timezone=True))
+    end_time = Column(DateTime(timezone=True))
+    task_description = Column(String(200))
+    duration_in_seconds = Column(Integer)
 
 Base.metadata.create_all(engine)
 
 Session = sessionmaker(bind=engine)
 session = Session()
 
-article1 = Article(
-    slug="clean-python",
-    title="How to Write Clean Python",
-    content="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+task1 = Task(
+    name="Vacuum"
 )
 
 
 def add_task():
-    session.add(article1)
+    session.add(task1)
     session.commit()
 
 add_task()
